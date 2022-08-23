ad94955561563a87d1a7c445138c7dbf53d53108
Astghik-90 <astgh1990@gmail.com>
2022-08-23 17:49:59 +0400

    initial commit

---
+++ Cengage/src/main/java/Locators/HomePageConstants.java
@@ -0,0 +1,7 @@
+package Locators;
+
+public class HomePageConstants {
+    public static final String signInButton = "sign-in";
+    public static final String username = "input[name='identifier']";
+    public static final String password = "input[name='password']";
+}
